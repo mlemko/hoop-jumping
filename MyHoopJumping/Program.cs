33f7686c34da15ec7858989b94c205da0b9c8348
mlemko <isamfz445@gmail.com>
2022-09-30 21:50:45 +0800

    Added feature: Network key select and information display.

--- MyHoopJumping/Program.cs
+++ MyHoopJumping/Program.cs
@@ -18,17 +18,44 @@ namespace MyHoopJumping
                 int i = 0;
                 foreach (string netkey in subkeys) 
                 {
-                    if (int.TryParse(netkey,out _)) 
+                    if (int.TryParse(netkey,out _))
                     {
                         networkKeys[i] = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\" + netkey);
-                        i++;
+                        if (networkKeys[i].GetValue("DriverDesc") != null) 
+                        {
+                            Console.WriteLine("{0} [{1}]", TruncateRight(networkKeys[i].GetValue("DriverDesc").ToString(), 49, 4).PadRight(49), i + 1);
+                            i++;
+                        }
                     }
                 }
+                Console.Write("Select a network driver: ");
+                int chosenKey = -20;
+                while (true) 
+                {
+                    if (int.TryParse(Console.ReadLine(), out chosenKey) && chosenKey - 1 <= i && chosenKey > 0) 
+                    {
+                        chosenKey -= 1;
+                        break;
+                    }
+                }
+                Console.Clear();
+                Console.WriteLine("Name: {0}", networkKeys[chosenKey].GetValue("DriverDesc"));
+                Console.WriteLine("Current MAC: {0}", networkKeys[chosenKey].GetValue("NetworkAddress"));
+                if (networkKeys[chosenKey].GetValue("NetworkAddress") == null) 
+                {
+                    Console.WriteLine("\nWarning!: The device selected has no existing MAC address! Creating one may cause the device to become unstable.");
+                }
             }
+            Console.ReadKey(true);
         }
-        static string TruncateRight(string str, int Maxlength) 
+        static string TruncateRight(string str, int Maxlength, int dots = 0) 
         {
-            return str.Substring(0, Math.Min(str.Length, Maxlength));
+            string newstr = str.Substring(0, Math.Min(str.Length, Maxlength - dots));
+            for (int i = 0; i >= dots; i++) 
+            {
+                newstr += ".";
+            }
+            return newstr;
         }
     }
 }
