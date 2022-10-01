ecfa50c8dc2747b83b11f415e19bd33ed0665e5b
mlemko <isamfz445@gmail.com>
2022-10-01 13:31:25 +0800

    Reworked networkKeys[] into a List, added loops.

--- MyHoopJumping/Program.cs
+++ MyHoopJumping/Program.cs
@@ -14,50 +14,59 @@ namespace MyHoopJumping
             if (basekey != null)
             {
                 string[] subkeys = basekey.GetSubKeyNames();
-                RegistryKey[] networkKeys = new RegistryKey[subkeys.Length];
+                List<RegistryKey> networkKeys = new List<RegistryKey>();
 
-                int i = 0;
-                foreach (string netkey in subkeys) 
+                foreach (string netkey in subkeys)
                 {
-                    if (int.TryParse(netkey,out _))
+                    if (int.TryParse(netkey, out _))
                     {
-                        networkKeys[i] = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\" + netkey);
-                        if (networkKeys[i].GetValue("DriverDesc") != null) 
+                        RegistryKey registry = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\" + netkey);
+                        if (registry.GetValue("DriverDesc") != null)
                         {
-                            Console.WriteLine("{0} [{1}]", TruncateRight(networkKeys[i].GetValue("DriverDesc").ToString(), 49, 4).PadRight(49), i + 1);
-                            i++;
+                            networkKeys.Add(registry);
                         }
                     }
                 }
-                Console.Write("Select a network driver: ");
-                int chosenKey = -20;
-                while (true) 
+                while (true)
                 {
-                    if (int.TryParse(Console.ReadLine(), out chosenKey) && chosenKey - 1 <= i && chosenKey > 0) 
+                select:
+                    Console.Clear();
+                    for (int i = 0; i < networkKeys.Count; i++)
                     {
-                        chosenKey -= 1;
-                        Console.Clear();
-                        Console.WriteLine("Name: {0}", networkKeys[chosenKey].GetValue("DriverDesc"));
-                        Console.WriteLine("Current MAC: {0}", networkKeys[chosenKey].GetValue("NetworkAddress"));
-                        if (networkKeys[chosenKey].GetValue("NetworkAddress") == null)
+                        Console.WriteLine("{0} [{1}]", TruncateRight(networkKeys[i].GetValue("DriverDesc").ToString(), 49, 3).PadRight(49), i + 1);
+                    }
+                    Console.Write("Select a network driver: ");
+                    int chosenKey = -20;
+                    while (true)
+                    {
+                        if (int.TryParse(Console.ReadLine(), out chosenKey) && chosenKey - 1 <= networkKeys.Count && chosenKey > 0)
                         {
-                            Console.WriteLine("\nWarning!: The device selected has no existing MAC address! Creating one may cause the device to become unstable.");
-                        }
-                        Console.WriteLine("\nWould you like to change/add a MAC address? (y/n)");
-                        while (true) 
-                        {
-                            switch (YesOrNo(Console.ReadLine())) 
+                            chosenKey -= 1;
+                            Console.Clear();
+                            Console.WriteLine("Name: {0}", networkKeys[chosenKey].GetValue("DriverDesc"));
+                            Console.WriteLine("Current MAC: {0}", networkKeys[chosenKey].GetValue("NetworkAddress"));
+                            if (networkKeys[chosenKey].GetValue("NetworkAddress") == null)
                             {
-                                case true:
-                                    MACRandomizer(networkKeys[chosenKey]);
-                                    break;
-                                default:
-                                    break;
+                                Console.WriteLine("\nWarning!: The device selected has no existing MAC address! Creating one may cause the device to become unstable.");
+                            }
+                            Console.WriteLine("\nWould you like to change/add a MAC address? (y/n)");
+                            while (true)
+                            {
+                                switch (YesOrNo(Console.ReadLine()))
+                                {
+                                    case true:
+                                        MACRandomizer(networkKeys[chosenKey]);
+                                        goto select;
+                                    case false:
+                                        goto select;
+                                    default:
+                                        break;
+                                }
                             }
                         }
                     }
+
                 }
-                
             }
             Console.ReadKey(true);
         }
@@ -73,7 +82,8 @@ namespace MyHoopJumping
             Console.WriteLine("[Q] Quit");
             Console.WriteLine("[R] Randomize Now!");
             Console.WriteLine("[S] Settings");
-
+            Console.WriteLine();
+            Console.ReadLine();
             return 0;
         }
         static string TruncateRight(string str, int Maxlength, int dots = 0) 
